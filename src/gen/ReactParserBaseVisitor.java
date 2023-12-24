74ed49dd44b4ea0d407c7cc2962448abe54b58be
anasallahham <anastennis12@gmail.com>
2023-12-24 20:49:57 +0300

    ANAS gen

---
+++ src/gen/ReactParserBaseVisitor.java
@@ -0,0 +1,211 @@
+// Generated from C:/Users/anast/OneDrive/Desktop/COMPILER X/Compiler-2.1/ReactParser.g4 by ANTLR 4.13.1
+package gen;
+import org.antlr.v4.runtime.tree.AbstractParseTreeVisitor;
+
+/**
+ * This class provides an empty implementation of {@link ReactParserVisitor},
+ * which can be extended to create a visitor which only needs to handle a subset
+ * of the available methods.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+@SuppressWarnings("CheckReturnValue")
+public class ReactParserBaseVisitor<T> extends AbstractParseTreeVisitor<T> implements ReactParserVisitor<T> {
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitProgram(ReactParser.ProgramContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitImportStatment(ReactParser.ImportStatmentContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitVariableDeclaration(ReactParser.VariableDeclarationContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitRegularFunction(ReactParser.RegularFunctionContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCallBackFunction(ReactParser.CallBackFunctionContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitFunctionbody(ReactParser.FunctionbodyContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitCallFun(ReactParser.CallFunContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitReturnStatment(ReactParser.ReturnStatmentContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitReturnStatment1(ReactParser.ReturnStatment1Context ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitSyntax(ReactParser.SyntaxContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitPureReact(ReactParser.PureReactContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitAttributes(ReactParser.AttributesContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitAttribute(ReactParser.AttributeContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitAttributeValue(ReactParser.AttributeValueContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitPrint(ReactParser.PrintContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitUseRef(ReactParser.UseRefContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitUseState(ReactParser.UseStateContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitArray(ReactParser.ArrayContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitFunctionCall(ReactParser.FunctionCallContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitUseEffet(ReactParser.UseEffetContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitArray_content(ReactParser.Array_contentContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitMap(ReactParser.MapContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitOnClick(ReactParser.OnClickContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitHtml(ReactParser.HtmlContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitHtmlBody(ReactParser.HtmlBodyContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitHtmlhtml(ReactParser.HtmlhtmlContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitMapMethod(ReactParser.MapMethodContext ctx) { return visitChildren(ctx); }
+	/**
+	 * {@inheritDoc}
+	 *
+	 * <p>The default implementation returns the result of calling
+	 * {@link #visitChildren} on {@code ctx}.</p>
+	 */
+	@Override public T visitExport(ReactParser.ExportContext ctx) { return visitChildren(ctx); }
+}
---
+++ src/gen/ReactParserVisitor.java
@@ -0,0 +1,181 @@
+// Generated from C:/Users/anast/OneDrive/Desktop/COMPILER X/Compiler-2.1/ReactParser.g4 by ANTLR 4.13.1
+package gen;
+import org.antlr.v4.runtime.tree.ParseTreeVisitor;
+
+/**
+ * This interface defines a complete generic visitor for a parse tree produced
+ * by {@link ReactParser}.
+ *
+ * @param <T> The return type of the visit operation. Use {@link Void} for
+ * operations with no return type.
+ */
+public interface ReactParserVisitor<T> extends ParseTreeVisitor<T> {
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#program}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitProgram(ReactParser.ProgramContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#importStatment}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitImportStatment(ReactParser.ImportStatmentContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#variableDeclaration}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitVariableDeclaration(ReactParser.VariableDeclarationContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#regularFunction}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitRegularFunction(ReactParser.RegularFunctionContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#callBackFunction}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCallBackFunction(ReactParser.CallBackFunctionContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#functionbody}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFunctionbody(ReactParser.FunctionbodyContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#callFun}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitCallFun(ReactParser.CallFunContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#returnStatment}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitReturnStatment(ReactParser.ReturnStatmentContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#returnStatment1}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitReturnStatment1(ReactParser.ReturnStatment1Context ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#syntax}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitSyntax(ReactParser.SyntaxContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#pureReact}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPureReact(ReactParser.PureReactContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#attributes}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAttributes(ReactParser.AttributesContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#attribute}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAttribute(ReactParser.AttributeContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#attributeValue}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitAttributeValue(ReactParser.AttributeValueContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#print}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitPrint(ReactParser.PrintContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#useRef}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitUseRef(ReactParser.UseRefContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#useState}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitUseState(ReactParser.UseStateContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#array}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArray(ReactParser.ArrayContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#functionCall}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitFunctionCall(ReactParser.FunctionCallContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#useEffet}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitUseEffet(ReactParser.UseEffetContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#array_content}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitArray_content(ReactParser.Array_contentContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#map}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitMap(ReactParser.MapContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#onClick}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitOnClick(ReactParser.OnClickContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#html}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitHtml(ReactParser.HtmlContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#htmlBody}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitHtmlBody(ReactParser.HtmlBodyContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#htmlhtml}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitHtmlhtml(ReactParser.HtmlhtmlContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#mapMethod}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitMapMethod(ReactParser.MapMethodContext ctx);
+	/**
+	 * Visit a parse tree produced by {@link ReactParser#export}.
+	 * @param ctx the parse tree
+	 * @return the visitor result
+	 */
+	T visitExport(ReactParser.ExportContext ctx);
+}
